910ba27880e0bacc93a43ddcb87d349be8e710ca
fboccassi <francis8831@gmail.com>
2015-11-27 23:04:43 +0100

    singoloEvento

--- www/js/app.js
+++ www/js/app.js
@@ -60,6 +60,16 @@ angular.module('starter', ['ionic', 'dionics.dataService', 'dionics.neiDintorniC
       }
   })
 
+  .state('app.evento', {
+      url: '/singoloevento/:evento',
+      views: {
+        '@': {
+          templateUrl: 'views/eventi/neiDintorni/neiDintorni.html',
+          controller: 'NeiDintorniCtrl'
+        }
+      }
+  })
+
   ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/eventi/neidintorni');
